a1b48bf77c6c387223403788b840b6dbfcef0941
Kateryna Honchar <katerynahon4ar@gmail.com>
2025-02-15 18:30:51 +0200

    refactor(extended-1.2): specify alternative content for multimedia

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="button__image";
+    deleteButtonImg.alt="delete button";
     deleteButton.appendChild(deleteButtonImg);
 
 
